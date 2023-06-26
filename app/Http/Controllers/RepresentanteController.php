0dab81acd65be03d5330f749ba42119317ed908b
Oscar Ayala <fabianayala675@gmail.com>
2023-06-26 02:56:26 -0400

    instalacion de composer

--- app/Http/Controllers/RepresentanteController.php
+++ app/Http/Controllers/RepresentanteController.php
@@ -19,7 +19,7 @@ class RepresentanteController extends Controller
 
     public function create()
     {
-        return view('formularioVista.crearRepresentante');
+        return view('formularioVista.crearRepresentante ');
     }
 
     public function store(Request $request)
