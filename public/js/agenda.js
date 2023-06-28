26076ffa70b02f3fe53a984b4a898d99a3b7f028
Oscar Ayala <fabianayala675@gmail.com>
2023-06-27 23:18:57 -0400

    Realizar cambios en agenda

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,25 +1,18 @@
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('agenda');
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    
-   
-    
-    initialView: 'dayGridMonth',
-    locale:"es",
-
-    
-    
-    headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right:'dayGridMonth,timeGridWeek,listWeek'
-      
-    },
+      initialView: 'dayGridMonth',
+      locale: 'es',
+      headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,listWeek',
+      },
+      dateClick: function(info) {
+          $("#evento").modal("show");
+      }
   });
   calendar.render();
 });
+
 
--- resources/views/producto/agenda.blade.php
+++ resources/views/producto/agenda.blade.php
@@ -2,9 +2,46 @@
 @section('content')
 
 <div class="container">
-        <div id="agenda">
-            
-        </div>
+    <div id="agenda">
+        calendario
+    </div>
 </div>
+
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#evento">
+        Launch 
+</button>
+
+
+<!-- Modal -->
+<div class="modal fade" id="evento" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Body
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+
+@section('scripts')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+<script src="{{ asset('fullcalendar/fullcalendar.min.js') }}"></script>
+
+<link rel="stylesheet" href="{{ asset('fullcalendar/fullcalendar.min.css') }}" />
 
 @endsection
